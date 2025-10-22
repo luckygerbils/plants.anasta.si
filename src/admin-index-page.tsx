b1e94800cf33e34e0572e2ab1c874d9f33ef23c3
Sean Anastasi <sean@anasta.si>
2025-10-21 17:55:46 -0700

    Arrange plants in a grid instead of a list

--- src/admin-index-page.tsx
+++ src/admin-index-page.tsx
@@ -145,21 +145,21 @@ function AdminIndexPageInternal({
         </div>
       )}
       {matchingPlants.length > 0 && (
-        <ul>
+        <ul className="locations">
           {Array.from(plantsByLocation.keys()).sort().map(location => (
             <li key={location}>
               <div className="location">{location}</div>
-              <ul>
+              <ul className="plants">
                 {plantsByLocation.get(location)!.sort(comparing(p => p.name, localeCompare)).map(plant => {
                   const latestPhotoId = (plant.photos ?? []).sort(comparing(p => p.modifyDate, reversed(localeCompare)))?.[0]?.id;
                   return (
                     <li key={plant.id} className="plant">
                       <a href={`/admin/plant?plantId=${plant.id}`} id={plant.id}>
-                        {plant.name}
                         {latestPhotoId &&
                           <PhotoImg
                             loading="lazy" sizes="50vw"
                             photoId={`${plant.id}/${latestPhotoId}`} />}
+                        <div className="name">{plant.name}</div>
                       </a>
                     </li>
                   );
--- src/public-index-page.tsx
+++ src/public-index-page.tsx
@@ -16,21 +16,21 @@ export function PublicIndexPage({
     }, new Map<string, Plant[]>());
 
   return (
-    <ul>
+    <ul className="locations">
       {Array.from(plantsByLocation.keys()).sort().map(location => (
         <li key={location}>
           <div className="location">{location}</div>
-          <ul>
+          <ul className="plants">
             {plantsByLocation.get(location)!.sort(comparing(p => p.name, localeCompare)).map(plant => {
               const latestPhotoId = plant.photos.sort(comparing(p => p.modifyDate, reversed(localeCompare)))?.[0]?.id;
               return (
                 <li key={plant.id} className="plant">
                   <a href={`/${plant.id}`} id={plant.id}>
-                    {plant.name}
                     {latestPhotoId &&
                       <PhotoImg
                         loading="lazy" sizes="50vw"
                         photoId={`${plant.id}/${latestPhotoId}`} />}
+                    <div className="name">{plant.name}</div>
                   </a>
                 </li>
               );
